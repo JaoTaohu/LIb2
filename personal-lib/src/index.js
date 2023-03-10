a00088923869534214db6e74ddde84d1ade3eed5
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2023-03-10 20:34:36 +0700

    try to route to upload

--- personal-lib/src/index.js
+++ personal-lib/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignUp from './signup';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Upload from './upload';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path="Upload" element={<Upload/>}/>
+        <Route path="Signup" element={<SignUp/>}/>
+      </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ personal-lib/src/signup.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { redirect } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+import Upload from "./upload";
+
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [redirectToUpload, setRedirectToUpload] = useState(false);
+
+  const handleSignup = async (event) => {
+    event.preventDefault();
+    setError(null);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setRedirectToUpload(true); // set state to redirect to upload page
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  if (redirectToUpload) {
+    return redirect("/upload") // redirect to upload page
+  }
+
+  return (
+    <form onSubmit={handleSignup}>
+    <div>
+      <h2>Sign Up</h2>     
+        <label>Email</label>
+        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label>Password</label>
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type='submit' >Sign Up</button>        
+    </div>
+    </form>
+  );
+}
+
+export default Signup;
